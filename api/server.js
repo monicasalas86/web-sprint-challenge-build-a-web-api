d5d5249365d59a6a8b277e4df2d3aebc9787d58e
monicasalas86 <monicsalas167@gmail.com>
2021-10-29 11:21:12 -0500

    connected actions router to server.js

--- api/server.js
+++ api/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const server = express();
+const actionsRouter = require('./actions/actions-router');
+server.use(express.json());
 
-// Configure your server here
 // Build your actions router in /api/actions/actions-router.js
+server.use('/api/actions', actionsRouter);
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>Hello from Server.js</h1>
+    `)
+})
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
